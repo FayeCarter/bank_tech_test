3c96cb3f44774359d5f4ca9ae0ee8923b5047c24
FayeCarter <fayecarter333@gmail.com>
2020-05-18 16:13:08 +0100

    refactor: statement, redundant methods replaced with attr_reader

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -1,22 +1,11 @@
 class Transaction
 
+  attr_reader :date, :debit, :credit
+  
   def initialize(credit: nil, debit: nil)
     @date = Time.now.strftime('%d/%m/%Y')
     @credit = credit
     @debit = debit
-  end
-
-  def date
-    @date
-  end
-
-  def credit
-    @credit
-  end
-
-
-  def debit
-    @debit
   end
 
 end
