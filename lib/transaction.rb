2b452ff2a81b57714efb51ba1700d550f8c7b970
FayeCarter <fayecarter333@gmail.com>
2020-05-18 15:30:02 +0100

    feat: credit initializes a transaction

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -1,5 +1,9 @@
 class Transaction
 
+  def initialize(credit)
+    @credit = credit
+  end
+
   def date
     Time.now.strftime('%d/%m/%Y')
   end
--- spec/unit/transaction_spec.rb
+++ spec/unit/transaction_spec.rb
@@ -3,9 +3,14 @@ require 'transaction'
 describe Transaction do 
 
   today = Time.now.strftime('%d/%m/%Y')
-  
+
   it 'has a date' do
-    transaction = Transaction.new
+    transaction = Transaction.new(credit: 1)
     expect(transaction.date).to eq today
+  end
+
+  it 'can be initialized with credit' do
+    transaction = Transaction.new(credit: 5.00)
+    expect(transaction).to be
   end
 end
