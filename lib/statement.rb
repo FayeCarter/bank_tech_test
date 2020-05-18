4dd4726024453bd3f75348971320bcc5022124b1
FayeCarter <fayecarter333@gmail.com>
2020-05-18 17:56:24 +0100

    feat: Statement.format converts transaction to formatted transaction

--- lib/statement.rb
+++ lib/statement.rb
@@ -4,6 +4,15 @@ class Statement
     @header = "date || credit || debit || balance"
   end
 
+  def format(transactions)
+    display = []
+    transactions.each { |record|
+      transaction = "#{record.date} || #{record.credit} || #{record.debit} || #{record.balance}"
+      display.push(transaction)
+    }
+    display
+  end
+
   def print
     @header
   end
--- spec/unit/statement_spec.rb
+++ spec/unit/statement_spec.rb
@@ -6,5 +6,18 @@ describe Statement do
     statement = Statement.new
     expect(statement.print).to eq "date || credit || debit || balance"
   end
-  
+
+  describe "#format" do
+    it 'takes transactions and adds formatting' do
+      statement = Statement.new
+      transaction = double(
+        date: '18/05/2020',
+        credit: '100.00',
+        debit: nil,
+        balance: '200.00'
+      )
+      expect(statement.format([transaction])).to eq ["18/05/2020 || 100.00 ||  || 200.00"]
+    end
+  end
+
 end
