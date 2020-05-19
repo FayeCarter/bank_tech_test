f01cdd9ec63decaebe5adeecb5b36067ba0e7f84
FayeCarter <fayecarter333@gmail.com>
2020-05-19 11:48:04 +0100

    refactor: Statement spec subject and double introduced to better orbagnise tests

--- spec/unit/statement_spec.rb
+++ spec/unit/statement_spec.rb
@@ -1,38 +1,22 @@
 require 'statement'
 
 describe Statement do
-  
+
+  subject(:statement) { described_class.new }
+  let(:transaction) { double(:transaction, date: '18/05/2020', credit: '100.00', debit: nil, balance: '200.00') }
+
   it 'initializes with column names' do
-    statement = Statement.new
     expect(statement.print).to eq "date || credit || debit || balance"
   end
 
-  describe "#format" do
-    it 'takes transactions and adds formatting' do
-      statement = Statement.new
-      transaction = double(
-        date: '18/05/2020',
-        credit: '100.00',
-        debit: nil,
-        balance: '200.00'
-      )
-      expect(statement.format([transaction])).to eq ["18/05/2020 || 100.00 ||  || 200.00"]
-    end
+  it 'takes transactions and adds formatting' do
+    expect(statement.format([transaction])).to eq ["18/05/2020 || 100.00 ||  || 200.00"]
   end
 
-  describe '#print' do
-    it 'prints a transaction on a row underneath the header' do
-      statement = Statement.new
-      transaction = double(
-        date: '18/05/2020',
-        credit: '100.00',
-        debit: nil,
-        balance: '200.00'
-      )
-      statement.format([transaction])
-      expect(statement.print).to eq 'date || credit || debit || balance
+  it 'prints a transaction on a row underneath the header' do
+    statement.format([transaction])
+    expect(statement.print).to eq 'date || credit || debit || balance
 18/05/2020 || 100.00 ||  || 200.00'
-    end
   end
 
 end
