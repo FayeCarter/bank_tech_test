6a02c8d78c0388055fd8173ea070a8708bc98554
FayeCarter <fayecarter333@gmail.com>
2020-05-19 10:49:41 +0100

    feat: transaction Debit is float

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -5,14 +5,14 @@ class Transaction
   def initialize(credit: nil, debit: nil, balance:)
     @date = Time.now.strftime('%d/%m/%Y')
     @credit = float(credit)
-    @debit = debit
+    @debit = float(debit)
     @balance = calculate(balance, credit, debit)
   end
 
   private
 
   def float(value)
-    return value == nil ? value : format("%<balance>.2f", balance: value)
+    return value == nil ? value : format("%<value>.2f", value: value)
   end
 
   def calculate(balance, credit, debit)
--- spec/unit/transaction_spec.rb
+++ spec/unit/transaction_spec.rb
@@ -32,7 +32,7 @@ describe Transaction do
 
     it 'credit value is a float with 2 decimal places' do
       transaction = Transaction.new(balance: 100.00, credit: 5)
-      expect(transaction.credit).to eq format("%<balance>.2f", balance: 5)
+      expect(transaction.credit).to eq format("%<credit>.2f", credit: 5)
     end
   end
 
@@ -44,13 +44,18 @@ describe Transaction do
     it 'transaction can be initialized with debit' do
       transaction = Transaction.new(balance: 100.00, debit: 5.00)
       expect(transaction.credit).to eq nil
-      expect(transaction.debit).to eq 5.00
+      expect(transaction.debit).to  eq format("%<debit>.2f", debit: 5)
       expect(transaction.balance).to eq 95.00
     end
 
     it 'decreases the balance' do
       transaction = Transaction.new(balance: 100.00, debit: 5.00)
       expect(transaction.balance).to eq 95.00
+    end
+
+    it 'debit value is a float with 2 decimal places' do
+      transaction = Transaction.new(balance: 100.00, debit: 5)
+      expect(transaction.debit).to eq format("%<debit>.2f", debit: 5)
     end
   end
 end
