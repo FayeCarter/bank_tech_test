2385b4f273ff304640a260c7b81d2fb61f79b26f
FayeCarter <fayecarter333@gmail.com>
2020-05-19 08:58:45 +0100

    chore: move account_spec into correct folder

--- spec/unit/account_spec.rb
+++ spec/unit/account_spec.rb
@@ -1,20 +1,59 @@
 require 'account'
+require 'transaction'
 
 describe Account do
+  subject(:account) { described_class.new } 
+  today = Time.now.strftime('%d/%m/%Y')
+
   it 'Can be created with initial balance of £0.00' do
-    account = Account.new
     expect(account.display_balance).to eq "£0.00"
   end
 
-  it 'Balance is increased with a deposit' do
-    account = Account.new
-    account.deposit(5.00)
-    expect(account.display_balance).to eq "£5.00"
+  describe "#deposit" do
+    it 'increases the balance' do
+      account.deposit(5.00)
+      expect(account.display_balance).to eq "£5.00"
+    end
+
+    it 'increases account transaction_history' do
+      account.deposit(5.00)
+      expect(account.transaction_history.length).to eq 1
+    end
+
+    it 'creates a transaction' do
+      account.deposit(5.00)
+      expect(account.transaction_history[0]).to be_a Transaction
+    end
   end
 
-  it 'making a deposit increases account transactions' do
-    account = Account.new
-    account.deposit(5.00)
-    expect(account.transaction_history.length).to eq 1
+  describe "#withdraw" do
+    it 'decreases the balance' do
+      account.deposit(25.00)
+      account.withdraw(5.00)
+      expect(account.display_balance).to eq "£20.00"
+    end
+
+    it 'increases account transaction_history' do
+      account.deposit(25.00)
+      account.withdraw(5.00)
+      expect(account.transaction_history.length).to eq 2
+    end
+
+    it 'creates a transaction' do
+      account.deposit(25.00)
+      account.withdraw(5.00)
+      expect(account.transaction_history[1]).to be_a Transaction
+    end
+  end
+
+  describe "#print_statement" do
+    it 'returns an empty statement for a new account' do
+      expect(account.print_statement).to eq "date || credit || debit || balance"
+    end
+
+    xit 'returns credit history when deposit is made' do
+      account.deposit(5.00)
+      expect(account.print_statement).to eq "date || credit || debit || balance\n#{today} || || 5.00 || 5.00"
+    end
   end
 end
