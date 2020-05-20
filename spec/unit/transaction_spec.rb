d5bd0977547c252fe8e021a20a77a775f53359e7
FayeCarter <fayecarter333@gmail.com>
2020-05-20 16:17:24 +0100

    refactor - transaction tests remove state driven expectations

--- spec/unit/transaction_spec.rb
+++ spec/unit/transaction_spec.rb
@@ -2,52 +2,26 @@ require 'transaction'
 
 describe Transaction do 
 
-  today = Time.now.strftime('%d/%m/%Y')
-
+  
   it 'has a todays date by default' do
+    today = Time.now.strftime('%d/%m/%Y')
     transaction = Transaction.new(balance: 100)
+
     expect(transaction.date).to eq today
   end
 
-  it 'credit value is empty by default' do
+  it 'creates a credit value' do
+    transaction = Transaction.new(balance: 100, credit: 5)
+    expect(transaction.credit).to eq "5.00"
+  end
+
+  it 'creates a debit' do
+    transaction = Transaction.new(balance: 100, debit: 5)
+    expect(transaction.debit).to eq "5.00"
+  end
+
+  it 'creates a balance value' do
     transaction = Transaction.new(balance: 100)
-    expect(transaction.credit).to eq nil
-  end
-
-  it 'debit value is empty by default' do
-    transaction = Transaction.new(balance: 100)
-    expect(transaction.debit).to eq nil
-  end
-
-  it 'balance value is a float with 2 decimal places' do
-    transaction = Transaction.new(balance: 100)
-    expect(transaction.balance).to eq format("%<balance>.2f", balance: 100)
-  end
-
-  describe "#credit" do 
-
-    it 'transaction can be initialized with credit' do
-      transaction = Transaction.new(balance: 100, credit: 5)
-      expect(transaction.credit).to eq format("%<balance>.2f", balance: 5)
-    end
-
-    it 'credit value is a float with 2 decimal places' do
-      transaction = Transaction.new(balance: 100, credit: 5)
-      expect(transaction.credit).to eq format("%<credit>.2f", credit: 5)
-    end
-  end
-
-  describe "#debit" do 
-
-    it 'transaction can be initialized with debit' do
-      transaction = Transaction.new(balance: 100, debit: 5)
-      expect(transaction.debit).to eq format("%<debit>.2f", debit: 5)
-    end
-
-    it 'debit value is a float with 2 decimal places' do
-      transaction = Transaction.new(balance: 100, debit: 5)
-
-      expect(transaction.debit).to eq format("%<debit>.2f", debit: 5)
-    end
+    expect(transaction.balance).to eq "100.00"
   end
 end
