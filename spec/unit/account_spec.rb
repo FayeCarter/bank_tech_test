c3da8c1ec074db638a07461366c5500b89b8d2c3
FayeCarter <fayecarter333@gmail.com>
2020-05-21 11:36:11 +0100

    refactor: correct account_spec tests to model behaviour

--- spec/unit/account_spec.rb
+++ spec/unit/account_spec.rb
@@ -1,21 +1,26 @@
 require 'account'
 
-describe Account do
+describe 'Account' do
 
   describe "#deposit" do
     it 'depositing 5 returns a balance of 5.00' do
-      expect(subject.deposit(5)).to eq "Balance: 5.00"
+      account = Account.new
+      call = account.deposit(5)
+      expection = "Balance: 5.00"
+
+      expect(call).to eq expection
     end
 
     it 'adds a record to the transaction_history' do
-      subject.deposit(5)
-      expect(subject.transaction_history).to_not be_empty
+      account = Account.new
+      account.deposit(5)
+      expect(account.transaction_history).to_not be_empty
     end
 
     it 'uses the transaction class' do
       transaction = double(:transaction)
       allow(transaction).to receive(:new).with(balance: 5, credit: 5, debit: nil)
-      account = described_class.new(transaction: transaction)
+      account = Account.new(transaction: transaction)
 
       expect(transaction).to receive(:new)
       account.deposit(5)
@@ -24,18 +29,23 @@ describe Account do
 
   describe "#withdraw" do
     it 'withdrawing 5 returns a negative balance of -5.00' do
-      expect(subject.withdraw(5)).to eq "Balance: -5.00"
+      account = Account.new
+      call = account.withdraw(5)
+      expection = "Balance: -5.00"
+
+      expect(call).to eq expection
     end
 
     it 'adds a record to the transaction_history' do
-      subject.withdraw(5)
-      expect(subject.transaction_history).to_not be_empty
+      account = Account.new
+      account.withdraw(5)
+      expect(account.transaction_history).to_not be_empty
     end
 
     it 'uses the transaction class' do
       transaction = double(:transaction, new: "new transaction")
       allow(transaction).to receive(:new).with(balance: -5, credit: nil, debit: 5)
-      account = described_class.new(transaction: transaction)
+      account = Account.new(transaction: transaction)
 
       expect(transaction).to receive(:new)
       account.withdraw(5)
@@ -46,7 +56,7 @@ describe Account do
     it 'uses the statement class' do
       account_statement = double(:statement)
       statement_class = double(:statement_class, new: account_statement)
-      account = described_class.new(statement: statement_class)
+      account = Account.new(statement: statement_class)
       
       expect(account_statement).to receive(:create)
       account.print_statement
